b756a338c026d60a6f27fc45486d52a3be219d06
masaibar <orebatemoko@gmail.com>
2014-12-28 22:50:51 +0900

    webView作成、表示まで

--- app/src/main/java/masaibar/co/jp/mypluto/MainActivity.java
+++ app/src/main/java/masaibar/co/jp/mypluto/MainActivity.java
@@ -1,19 +1,58 @@
 package masaibar.co.jp.mypluto;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private WebView webView;
+    private EditText editTextUrl;
+//    private static final String URL_PLUTO = "https://pluto.io/sp/login.html";
+    private static final String URL_PLUTO = "https://pluto.io";
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        //ソフトウェアキーボードの出現を抑制
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        editTextUrl = (EditText) findViewById((R.id.editText));
+        webView = (WebView) findViewById(R.id.webView);
+
+        //リンク先もWebViewで開くようにする
+        webView.setWebViewClient(new WebViewClient());
+        //JavaScriptの実行を許可
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl(URL_PLUTO);
+
+        //webViewの表示しているURLを表示
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                editTextUrl.setText(webView.getOriginalUrl());
+            }
+        });
+
+    }
+    //戻るボタンの挙動をOverride
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
