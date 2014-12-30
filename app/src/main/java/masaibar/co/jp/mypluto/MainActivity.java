fc41e1420982ed19446196a381c5bbc05f01fb96
masaibar <orebatemoko@gmail.com>
2014-12-30 19:19:44 +0900

    通知部分とりあえず土台だけ

--- app/src/main/java/masaibar/co/jp/mypluto/MainActivity.java
+++ app/src/main/java/masaibar/co/jp/mypluto/MainActivity.java
@@ -1,6 +1,10 @@
 package masaibar.co.jp.mypluto;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -45,6 +49,7 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+        sendNotification();
     }
     //戻るボタンの挙動をOverride
     @Override
@@ -74,5 +79,31 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //あとで切り離す
+    public void sendNotification() {
+        //Notificationインスタンスの生成
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        //ステータスバーに表示する設定を行う
+        builder.setSmallIcon(android.R.drawable.star_big_on);
+        builder.setTicker("Do you need Pluto?");
+
+        //通知領域に表示する設定を行う
+        builder.setContentTitle("Plutoで家電を操作しますか？");
+        builder.setContentText("こちらから開けます");
+
+        //Intentの生成
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0 , intent, 0);
+        builder.setContentIntent(contentIntent);
+        builder.setWhen(System.currentTimeMillis());
+        //通知エリアに残す
+        builder.setAutoCancel(false);
+
+        //通知の実行
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0 , builder.build());
     }
 }
